21cbd982c965fd9f9f639efa164b0dcf37eaa472
Ana Jovanova <ana@vercel.com>
2025-02-27 13:07:55 +0100

    Update styles

--- app/claim/preview-url-icon.tsx
+++ app/claim/preview-url-icon.tsx
@@ -6,10 +6,10 @@ export default function PreviewUrlIcon({ previewUrl }: { previewUrl: string }) {
   return (
     <button
       type="button"
-      className="w-full flex items-center justify-between p-4 rounded-lg transition-colors duration-200 border bg-white border-gray-200 text-sm text-gray-700 hover:bg-gray-100"
+      className="w-full flex items-center gap-2 p-4 rounded-lg transition-colors duration-200 border bg-white border-gray-200 text-sm text-gray-700 hover:bg-gray-100"
       onClick={() => window.open(previewUrl, "_blank")}
     >
-      {previewUrl}
+      <span className="flex-1 text-left break-all">{previewUrl}</span>
       <Image
         alt="External link"
         src="icons/external-link.svg"
